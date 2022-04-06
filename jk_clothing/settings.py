6689070a37dc64aa61e3e54bcf69cfca39c4d840
ainchu18 <christianfordmacamchu18@gmail.com>
2022-04-06 15:30:30 +0000

    add crispy bootstrap 5

--- jk_clothing/settings.py
+++ jk_clothing/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'jk_clothing.urls'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
