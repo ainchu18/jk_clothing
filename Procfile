44d30f7838257f1c55307f385d87053403f684ef
ainchu18 <christianfordmacamchu18@gmail.com>
2022-04-10 18:11:06 +0000

    add allowed host

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jk_clothing.wsgi:application
--- jk_clothing/settings.py
+++ jk_clothing/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a9#i1z7s*k9ibibh%72egcj&pl)8*+!96)pk1%764-3b1or5mo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jk-clothing.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,13 +123,17 @@ WSGI_APPLICATION = 'jk_clothing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
