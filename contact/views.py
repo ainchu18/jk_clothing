90d528d3a4c85144203b5fd7a9485c5034f6ca5f
ainchu18 <christianfordmacamchu18@gmail.com>
2022-06-10 20:22:12 +0000

    migrate contact to heroku database

--- contact/views.py
+++ contact/views.py
@@ -5,16 +5,15 @@ from .models import Contact
 
 def contact_form(request):
     """A view where users/shoppers can leave a comment or questions"""
-
+    contact = Contact()
     if request.method == 'POST':
-        contact = Contact()
         name = request.POST.get('name')
         email = request.POST.get('email')
         subject = request.POST.get('subject')
-
         contact.name = name
         contact.email = email
         contact.subject = subject
+
         contact.save()
         messages.success(request, 'Thanks for contacting us! We will contact you as soon as possible.')
         return render(request, 'home/index.html')
