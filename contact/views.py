15659492d15e7d780d4eae183ea559fd6fc96d55
ainchu18 <christianfordmacamchu18@gmail.com>
2022-06-10 19:10:19 +0000

    add contact model and update views, and about us text

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Contact
+from django.contrib import messages
 
 
-def contact(request):
-    return render(request, 'contact/contact.html')
+def contactForm(request):
+
+    if request.method == 'POST':
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+
+        contact.name = name
+        contact.email = email
+        contact.subject = subject
+        contact.save()
+        messages.success(request, 'Thanks for contacting us! We will contact you as soon as possible.')
+        return render(request, 'home/index.html')
+    else:
+        return render(request, 'contact/contact.html')
