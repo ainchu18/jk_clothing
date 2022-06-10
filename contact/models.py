4932cae895400eb148ddf0483b72a1a1ba331a77
ainchu18 <christianfordmacamchu18@gmail.com>
2022-06-10 18:23:12 +0000

    add contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    subject = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
