35a6704ffa681486b0a6783c63df2a1fa6a7adf5
ainchu18 <christianfordmacamchu18@gmail.com>
2022-06-14 12:58:52 +0000

    finish add wishlist view function

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import Wishlist
 from products.models import Product
+from django.contrib import messages
 
 
 def view_wishlist(request):
     """A view to return to wishlist content page"""
+    wlist = Wishlist.objects.filter(user=request.user).order_by('-id')
 
-    return render(request, 'wishlist/wishlist.html')
+    return render(request, 'wishlist/wishlist.html', {'wlist': wlist})
 
 
 def add_wishlist(request):
@@ -18,9 +20,11 @@ def add_wishlist(request):
     checkw = Wishlist.objects.filter(product=product, user=request.user).count()
 
     if checkw > 0:
+        
         data = {
             'bool': False
         }
+
     else:
         wishlist = Wishlist.objects.create(
             product=product,
@@ -28,5 +32,9 @@ def add_wishlist(request):
         )
         data = {
             'bool': True
+            
         }
+        
     return JsonResponse(data)
+
+
